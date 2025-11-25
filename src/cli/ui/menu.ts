ebc9bace55c371a654bc0b4bf6bb8e3d79bb1a38
Gabriel Soares <gabrielsoacc@gmail.com>
2025-11-25 00:14:40 -0300

    translate: menu PT-BR -> EN-USA

--- src/cli/ui/menu.ts
+++ src/cli/ui/menu.ts
@@ -2,7 +2,13 @@ import terminalKit from "terminal-kit";
 const term = terminalKit.terminal;
 
 export async function mainMenu() {
-  const menu = ["Listar temas", "Preview", "Aplicar", "Gerar novo", "Sair"];
+  const menu = [
+    "List Themes",
+    "Preview",
+    "Apply",
+    "Generate new theme",
+    "Exit",
+  ];
 
   const res = await term.singleColumnMenu(menu).promise;
   return res.selectedIndex;
