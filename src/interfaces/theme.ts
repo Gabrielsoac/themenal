6d137386e3f0da561c360ce0e0814691ccc9e834
Gabriel Soares <gabrielsoacc@gmail.com>
2025-11-27 21:26:30 -0300

    feat: created interface Theme

---
+++ src/interfaces/theme.ts
@@ -0,0 +1,18 @@
+export interface Theme {
+  black: string;
+  red: string;
+  green: string;
+  yellow: string;
+  blue: string;
+  magenta: string;
+  cyan: string;
+  white: string;
+  brightBlack: string;
+  brightRed: string;
+  brightGreen: string;
+  brightYellow: string;
+  brightBlue: string;
+  brightMagenta: string;
+  brightCyan: string;
+  brightWhite: string;
+}
